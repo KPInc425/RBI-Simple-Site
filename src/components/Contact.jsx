37be46eb77caf5c219a9f55f35eaa370abb50958
KPInc425 <kpinc425@gmail.com>
2024-06-08 13:53:51 -0700

    implement stopgap email solution

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,44 +2,70 @@ import { useState } from "react";
 import ContactForm from "./ContactForm";
 
 const Contact = () => {
-  const [showForm, setShowForm] = useState(false);
-  // const successToasterRef = useRef < ToasterHandle > null;
-  // const errorToasterRef = useRef < ToasterHandle > null;
+  const [showForm, setShowForm] = useState(true);
+  const [showToasterSuccess, setShowToasterSuccess] = useState(false);
+  const [showToasterError, setShowToasterError] = useState(false);
   const [toasterText, setToasterText] = useState("");
 
   const handleSuccessToaster = () => {
-    // successToasterRef.current?.publish();
+    setShowToasterSuccess(true);
+    setTimeout(() => {
+      setShowToasterSuccess(false);
+      setToasterText("");
+    }, 3000);
   };
   const handleErrorToaster = () => {
-    // errorToasterRef.current?.publish();
+    setShowToasterError(true);
+    setTimeout(() => {
+      setShowToasterError(false);
+      setToasterText("");
+    }, 3000);
   };
 
   return (
     <div className="flex flex-col xl:flex-row gap-4 xl:gap-20 justify-around container mx-auto text-xl text-center xl:text-start p-4 pb-12 lg:p-12">
-      <div className="flex flex-col gap-5">
-        <h1 className="text-3xl font-bold">Contact</h1>
-        <p>sales@robotbuildersinc.com</p>
+      <div className="toast toast-top toast-center">
+        {showToasterSuccess && (
+          <div className="alert alert-success">
+            <span className="font-semibold text-success-content">
+              {toasterText}
+            </span>
+          </div>
+        )}
+        {showToasterError && (
+          <div className=" alert alert-error">
+            <span className="font-semibold text-error-content">
+              {toasterText}
+            </span>
+          </div>
+        )}
       </div>
-      {!showForm ? (
+      <div
+        id="contact"
+        className="flex flex-col gap-4 justify-center items-center w-full"
+      >
+        <h1 className="text-3xl font-bold">Contact Us</h1>
+        {!showForm ? (
           <button
-            className="btn btn-secondary"
+            className={`btn btn-primary rounded-lg p-4 ${
+              showForm ? "hidden" : ""
+            }`}
             onClick={() => setShowForm(!showForm)}
           >
-            {" "}
-            Contact Us{" "}
+            Get in touch
           </button>
-      ) : (
-        <ContactForm
-          showForm={showForm}
-          setShowForm={setShowForm}
-          setToasterText={setToasterText}
-          handleSuccessToaster={handleSuccessToaster}
-          handleErrorToaster={handleErrorToaster}
-        />
-      )}
-      {/* <Toaster ref={successToasterRef} bgColor={'rgb(90,106,79)'}>{toasterText}</Toaster>
-    <Toaster ref={errorToasterRef} bgColor={'crimson'}>{toasterText}</Toaster> */}
-
+        ) : (
+          <div className="flex justify-center xl:justify-self-end items-center order-1 mb-4 xl:mb-0 w-3/4 px-4">
+            <ContactForm
+              showForm={showForm}
+              setShowForm={setShowForm}
+              setToasterText={setToasterText}
+              handleSuccessToaster={handleSuccessToaster}
+              handleErrorToaster={handleErrorToaster}
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/layouts/shared/Header.jsx
+++ src/components/layouts/shared/Header.jsx
@@ -4,7 +4,7 @@ import RBIFullIcon from "../../shared/icons/RBIFullIcon";
 const Header = ({theme, handleThemeChange}) => {
   return (
     <header className="py-4">
-      <div className="absolute top-1 right-16 xl:right-4 xl:top-5 h-full">
+      <div className="absolute top-1 right-16 xl:right-8 xl:top-5 h-full">
         <ThemeController theme={theme} handleThemeChange={handleThemeChange} />
       </div>
       <div className="lg:h-16 lg:w-68 text-primary p-4 lg:p-0">
